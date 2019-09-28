ae69273cf90b10334384c8607e7da15fca5173d3
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-09-28 23:03:19 +0400

    Fixed templates coping

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ cp switchmap.service /etc/systemd/system/
 systemctl daemon-reload
 go build switchmap.go
 mv -f switchmap /var/www/switchmap/
-cp -r conf.json public/ private/ /var/www/switchmap/
+cp -r conf.json public/ private/ templates/ /var/www/switchmap/
 systemctl enable switchmap
 systemctl start switchmap
--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 
 go build switchmap.go
 mv -f switchmap /var/www/switchmap/
-cp -r conf.json public/ private/ /var/www/switchmap/
+cp -r conf.json public/ private/ templates/ /var/www/switchmap/
 systemctl restart switchmap
