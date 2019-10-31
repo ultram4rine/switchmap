2b1f28a18a62a7e7ee6fe1b28e446252f830fc06
ultram4rine <42237599+ultram4rine@users.noreply.github.com>
2019-10-31 15:12:09 +0400

    npm install in scripts

--- install.sh
+++ install.sh
@@ -5,5 +5,6 @@ systemctl daemon-reload
 go build switchmap.go
 mv -f switchmap /var/www/switchmap/
 cp -r switchmap.conf.json public/ private/ templates/ /var/www/switchmap/
+(cd /var/www/switchmap/public/js/; npm install)
 systemctl enable switchmap
 systemctl start switchmap
--- update.sh
+++ update.sh
@@ -3,4 +3,5 @@
 go build switchmap.go
 mv -f switchmap /var/www/switchmap/
 cp -r switchmap.conf.json public/ private/ templates/ /var/www/switchmap/
+(cd /var/www/switchmap/public/js/; npm install)
 systemctl restart switchmap
