7a5651eb2f484482677538dea25203497a383352
ultram4rine <ultramarine730@gmail.com>
2020-01-12 23:41:24 +0400

    Fixed scripts

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ cp switchmap.service /etc/systemd/system/
 systemctl daemon-reload
 go build switchmap.go
 mv -f switchmap /var/www/switchmap/
-cp -r switchmap.conf.json public/ private/ templates/ /var/www/switchmap/
+cp -r switchmap.conf.json schema.sql public/ private/ templates/ /var/www/switchmap/
 (cd /var/www/switchmap/public/js/; npm install)
 systemctl enable switchmap
 systemctl start switchmap
