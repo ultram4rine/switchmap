744a95374a0c50f93970a46858b5325ab1f11397
ultram4rine <ultramarine730@gmail.com>
2020-01-13 11:47:40 +0400

    Fixed path to config in scripts

--- update.sh
+++ update.sh
@@ -2,6 +2,6 @@
 
 go build switchmap.go
 mv -f switchmap /var/www/switchmap/
-cp -r switchmap.conf.json schema.sql public/ private/ templates/ /var/www/switchmap/
+cp -r switchmap.conf.toml schema.sql public/ private/ templates/ /var/www/switchmap/
 (cd /var/www/switchmap/public; npm install)
 systemctl restart switchmap
