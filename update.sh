244f55dffd23d2ff19f5b6a6915a3e112e46b8aa
ultram4rine <ultramarine730@gmail.com>
2020-03-25 11:19:33 +0400

    removed old go code

--- update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-go build switchmap.go
-mv -f switchmap /var/www/switchmap/
-cp -r switchmap.conf.toml schema.sql public/ private/ templates/ /var/www/switchmap/
-(cd /var/www/switchmap/public; npm install)
-systemctl restart switchmap
