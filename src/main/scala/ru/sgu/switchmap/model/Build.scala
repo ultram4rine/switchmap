b137d36fc1f945f9867b2eb7416ea517e8c5ee30
ultram4rine <ultramarine730@gmail.com>
2020-03-27 16:28:15 +0400

    removed unused TableQuery

--- src/main/scala/ru/sgu/switchmap/model/Build.scala
+++ src/main/scala/ru/sgu/switchmap/model/Build.scala
@@ -3,8 +3,6 @@ package ru.sgu.switchmap.model
 import slick.jdbc.PostgresProfile.api._
 
 trait BuildComponent {
-  lazy val Builds = TableQuery[Builds]
-
   class Builds {
     class Builds(tag: Tag) extends Table[Build](tag, "builds") {
       def id = column[Int]("id", O.PrimaryKey)
