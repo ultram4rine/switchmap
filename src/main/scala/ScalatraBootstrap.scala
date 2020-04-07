fceac83baee8801b816e6df4d4cf1a274c0b2428
ultram4rine <ultramarine730@gmail.com>
2020-04-07 21:38:05 +0400

    fixed connection to database

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,43 @@
-import ru.sgu.switchmap._
-import org.scalatra._
 import javax.servlet.ServletContext
+import org.scalatra._
+import org.slf4j.{Logger, LoggerFactory}
+import ru.sgu.switchmap.SwitchMapApp
+import ru.sgu.switchmap.model.{BuildComponent, FloorComponent, SwitchComponent}
+import slick.jdbc.PostgresProfile.api._
 
-class ScalatraBootstrap extends LifeCycle {
+import scala.concurrent.{Await, Future}
+
+class ScalatraBootstrap
+    extends LifeCycle
+    with BuildComponent
+    with FloorComponent
+    with SwitchComponent {
+  val logger: Logger = LoggerFactory.getLogger(getClass)
+
+  val db = Database.forConfig("switchmap")
+
+  val createTables = DBIO.seq(
+    (builds.schema ++ floors.schema ++ switches.schema).createIfNotExists,
+    builds += (Build(1, "9 корпус", "b9"))
+  )
+
   override def init(context: ServletContext) {
-    context.mount(new MyScalatraServlet, "/*")
+    try {
+      db.run(createTables)
+    } catch {
+      case ex: Exception => logger.error(ex.getMessage())
+    }
+
+    context.mount(new SwitchMapApp(db), "/*")
+  }
+
+  private def closeDbConnection() {
+    logger.info("Closing DB connection")
+    db.close
+  }
+
+  override def destroy(context: ServletContext) {
+    super.destroy(context)
+    closeDbConnection
   }
 }
