c601edf1e34ee0323991798ca30bf066e60ab093
ultram4rine <ultramarine730@gmail.com>
2020-06-15 11:32:43 +0400

    fixed getting builds

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -1,202 +1,196 @@
 <template>
-    <div id="home">
-        <v-row no-gutters dense>
-            <v-col v-if="isLoading" :cols="3">
-                <v-skeleton-loader type="article" class="mx-auto"></v-skeleton-loader>
-            </v-col>
-            <v-col v-else v-for="build in builds" :key="build.addr" :cols="3">
-                <v-card class="ma-1" outlined>
-                    <v-list-item two-line>
-                        <v-list-item-content>
-                            <v-list-item-title class="headline mb-1">{{ build.name }}</v-list-item-title>
-                            <v-list-item-subtitle>{{ build.floorNumber }} floors, {{ build.switchNumber }} switches
-                            </v-list-item-subtitle>
-                        </v-list-item-content>
-                    </v-list-item>
+  <div id="home">
+    <v-row no-gutters dense>
+      <v-col v-if="isLoading" :cols="3">
+        <v-skeleton-loader type="article" class="mx-auto"></v-skeleton-loader>
+      </v-col>
+      <v-col v-else v-for="build in builds" :key="build.addr" :cols="3">
+        <v-card class="ma-1" outlined>
+          <v-list-item two-line>
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1">{{ build.name }}</v-list-item-title>
+              <v-list-item-subtitle>{{ build.floorNumber }} floors, {{ build.switchNumber }} switches</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
 
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn
-                                dark
-                                small
-                                color="primary"
-                                @click="addFloorForm = !addFloorForm; floorBuildName = build.name; floorBuildAddr = build.addr"
-                        >Add floor
-                        </v-btn>
-                        <v-btn
-                                dark
-                                small
-                                color="primary"
-                                :to="{ name: 'build', params: { addr: build.addr }}"
-                        >Go
-                        </v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-col>
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              dark
+              small
+              color="primary"
+              @click="addFloorForm = !addFloorForm; floorBuildName = build.name; floorBuildAddr = build.addr"
+            >Add floor</v-btn>
+            <v-btn
+              dark
+              small
+              color="primary"
+              :to="{ name: 'build', params: { addr: build.addr }}"
+            >Go</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
 
-            <v-col v-if="!isLoading && builds.length===0" :cols="3">
-                <v-card class="ma-1" outlined>
-                    <v-list-item>
-                        <v-list-item-content>
-                            <v-list-item-title class="headline mb-1">No builds to show</v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-card>
-            </v-col>
-        </v-row>
+      <v-col v-if="!isLoading && builds.length===0" :cols="3">
+        <v-card class="ma-1" outlined>
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title class="headline mb-1">No builds to show</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-card>
+      </v-col>
+    </v-row>
 
-        <v-row no-gutters>
-            <v-card class="ma-1">
-                <v-btn color="error" @click="addBuildForm = !addBuildForm">Add build</v-btn>
-            </v-card>
-        </v-row>
+    <v-row no-gutters>
+      <v-card class="ma-1">
+        <v-btn color="error" @click="addBuildForm = !addBuildForm">Add build</v-btn>
+      </v-card>
+    </v-row>
 
-        <v-snackbar v-model="snackbar" :timeout="timeout">
-            {{ item }} added!
-            <v-btn fab x-small @click="snackbar = false">
-                <v-icon dark>{{ mdiClose }}</v-icon>
-            </v-btn>
-        </v-snackbar>
+    <v-snackbar v-model="snackbar" :timeout="timeout">
+      {{ item }} added!
+      <v-btn fab x-small @click="snackbar = false">
+        <v-icon dark>{{ mdiClose }}</v-icon>
+      </v-btn>
+    </v-snackbar>
 
-        <v-dialog :value="addBuildForm" max-width="500px">
-            <v-card dark>
-                <v-toolbar>
-                    <v-toolbar-title>New build</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-btn icon @click="addBuildForm = false; buildName = ''; buildAddr = ''">
-                        <v-icon>{{ mdiClose }}</v-icon>
-                    </v-btn>
-                </v-toolbar>
-                <v-card-text>
-                    <v-form ref="form">
-                        <v-text-field v-model="buildName" label="Name" color="orange accent-2" required></v-text-field>
-                        <v-text-field v-model="buildAddr" label="Address" color="orange accent-2"
-                                      required></v-text-field>
-                    </v-form>
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="orange darken-1" @click="addBuild">Add</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+    <v-dialog :value="addBuildForm" max-width="500px">
+      <v-card dark>
+        <v-toolbar>
+          <v-toolbar-title>New build</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn icon @click="addBuildForm = false; buildName = ''; buildAddr = ''">
+            <v-icon>{{ mdiClose }}</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-card-text>
+          <v-form ref="form">
+            <v-text-field v-model="buildName" label="Name" color="orange accent-2" required></v-text-field>
+            <v-text-field v-model="buildAddr" label="Address" color="orange accent-2" required></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="orange darken-1" @click="addBuild">Add</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
-        <v-dialog v-model="addFloorForm" max-width="500px">
-            <v-card dark>
-                <v-toolbar>
-                    <v-toolbar-title>New floor</v-toolbar-title>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                            icon
-                            @click="addFloorForm = false; floorNumber = ''; floorBuildName = ''; floorBuildAddr = ''"
-                    >
-                        <v-icon>{{ mdiClose }}</v-icon>
-                    </v-btn>
-                </v-toolbar>
-                <v-card-text>
-                    <v-form ref="form">
-                        <v-text-field
-                                type="number"
-                                v-model="floorNumber"
-                                label="Number"
-                                color="orange accent-2"
-                                required
-                        ></v-text-field>
-                    </v-form>
-                </v-card-text>
-                <v-divider></v-divider>
-                <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="orange darken-1" @click="addFloor">Add</v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
-    </div>
+    <v-dialog v-model="addFloorForm" max-width="500px">
+      <v-card dark>
+        <v-toolbar>
+          <v-toolbar-title>New floor</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn
+            icon
+            @click="addFloorForm = false; floorNumber = ''; floorBuildName = ''; floorBuildAddr = ''"
+          >
+            <v-icon>{{ mdiClose }}</v-icon>
+          </v-btn>
+        </v-toolbar>
+        <v-card-text>
+          <v-form ref="form">
+            <v-text-field
+              type="number"
+              v-model="floorNumber"
+              label="Number"
+              color="orange accent-2"
+              required
+            ></v-text-field>
+          </v-form>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="orange darken-1" @click="addFloor">Add</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
 </template>
 
 <script lang="ts">
-    import Vue from "vue";
-    import axios from "axios";
-    import {mdiClose} from "@mdi/js";
+import Vue from "vue";
+import axios, { AxiosResponse } from "axios";
+import { mdiClose } from "@mdi/js";
 
-    import config from "../config/config";
-    import {Build, Builds} from "../types";
+import config from "../config/config";
+import { Build, Builds } from "../types";
 
-    export default Vue.extend({
-        props: {
-            isLoading: {type: Boolean, required: true}
-        },
+export default Vue.extend({
+  props: {
+    isLoading: { type: Boolean, required: true }
+  },
 
-        data() {
-            return {
-                mdiClose: mdiClose,
+  data() {
+    return {
+      mdiClose: mdiClose,
 
-                snackbar: false,
-                timeout: 3000,
+      snackbar: false,
+      timeout: 3000,
 
-                item: "",
+      item: "",
 
-                builds: [] as Array<Build>,
-                buildsEndpoint: `${config.apiURL}/builds`,
+      builds: new Array<Build>(),
+      buildsEndpoint: `${config.apiURL}/builds`,
 
-                addBuildForm: false,
-                addBuildEndpoint: `${config.apiURL}/build`,
-                buildName: "",
-                buildAddr: "",
+      addBuildForm: false,
+      addBuildEndpoint: `${config.apiURL}/build`,
+      buildName: "",
+      buildAddr: "",
 
-                addFloorForm: false,
-                addFloorFormEndpoint: `${config.apiURL}/floor`,
-                floorNumber: "",
-                floorBuildName: "",
-                floorBuildAddr: ""
-            };
-        },
+      addFloorForm: false,
+      addFloorFormEndpoint: `${config.apiURL}/floor`,
+      floorNumber: "",
+      floorBuildName: "",
+      floorBuildAddr: ""
+    };
+  },
 
-        created() {
-            this.getAllBuilds();
-        },
+  created() {
+    this.getAllBuilds();
+  },
 
-        methods: {
-            getAllBuilds() {
-                axios
-                    .get<Builds>(this.buildsEndpoint, {
-                        transformResponse: (r: any) => r.data
-                    })
-                    .then(resp => (this.builds = resp.data.builds))
-                    .catch(err => console.log(err));
-            },
+  methods: {
+    getAllBuilds() {
+      axios
+        .get<Build, AxiosResponse<Build[]>>(this.buildsEndpoint)
+        .then(resp => (this.builds = resp.data))
+        .catch(err => console.log(err));
+    },
 
-            addBuild() {
-                axios
-                    .post(this.addBuildEndpoint, {
-                        name: this.buildName,
-                        addr: this.buildAddr
-                    })
-                    .then(() => {
-                        this.getAllBuilds();
-                        this.addBuildForm = false;
-                        this.item = this.buildName;
-                        this.snackbar = true;
-                    })
-                    .catch(err => console.log(err));
-            },
+    addBuild() {
+      axios
+        .post(this.addBuildEndpoint, {
+          name: this.buildName,
+          addr: this.buildAddr
+        })
+        .then(() => {
+          this.getAllBuilds();
+          this.addBuildForm = false;
+          this.item = this.buildName;
+          this.snackbar = true;
+        })
+        .catch(err => console.log(err));
+    },
 
-            addFloor() {
-                axios
-                    .post(this.addFloorFormEndpoint, {
-                        number: parseInt(this.floorNumber, 10),
+    addFloor() {
+      axios
+        .post(this.addFloorFormEndpoint, {
+          number: parseInt(this.floorNumber, 10),
 
-                        buildName: this.floorBuildName,
-                        buildAddr: this.floorBuildAddr
-                    })
-                    .then(() => {
-                        this.addFloorForm = false;
-                        this.item = `${this.floorNumber} floor in ${this.buildName}`;
-                        this.snackbar = true;
-                    })
-                    .catch(err => console.log(err));
-            }
-        }
-    });
+          buildName: this.floorBuildName,
+          buildAddr: this.floorBuildAddr
+        })
+        .then(() => {
+          this.addFloorForm = false;
+          this.item = `${this.floorNumber} floor in ${this.buildName}`;
+          this.snackbar = true;
+        })
+        .catch(err => console.log(err));
+    }
+  }
+});
 </script>
