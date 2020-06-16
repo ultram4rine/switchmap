00489ee40b8f701163656700a2ba87b37658476b
ultram4rine <ultramarine730@gmail.com>
2020-06-17 00:21:23 +0400

    fmt

--- app/auth/ldap/LDAPInfo.scala
+++ app/auth/ldap/LDAPInfo.scala
@@ -2,8 +2,5 @@ package auth.ldap
 
 import com.mohiva.play.silhouette.api.AuthInfo
 
-case class LDAPInfo(
-  code: String,
-  username: String,
-  password: String
-) extends AuthInfo
+case class LDAPInfo(code: String, username: String, password: String)
+    extends AuthInfo
