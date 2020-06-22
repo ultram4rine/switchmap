17b728b649c578286851be090c523897dd31f29c
ultram4rine <ultramarine730@gmail.com>
2020-06-22 13:14:50 +0400

    removed unnecessary mixins property

--- ui/src/components/Home.vue
+++ ui/src/components/Home.vue
@@ -126,8 +126,6 @@ export default mixins(buildsMixin, floorsMixin).extend({
     isLoading: { type: Boolean, required: true }
   },
 
-  mixins: [buildsMixin, floorsMixin],
-
   data() {
     return {
       mdiClose: mdiClose
