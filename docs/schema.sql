44c0acf57449d49b052fda5af901e5f18aad15a4
ultram4rine <ultramarine730@gmail.com>
2020-07-06 11:16:13 +0400

    added actions

--- docs/schema.sql
+++ docs/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS floors
     build_name       STRING NOT NULL,
     build_short_name STRING NOT NULL,
     PRIMARY KEY (build_short_name, number),
-    CONSTRAINT fk_build FOREIGN KEY (build_name, build_short_name) REFERENCES builds (name, short_name)
+    CONSTRAINT fk_build FOREIGN KEY (build_name, build_short_name) REFERENCES builds (name, short_name) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 CREATE TABLE IF NOT EXISTS switches
@@ -30,6 +30,6 @@ CREATE TABLE IF NOT EXISTS switches
     up_switch_mac    STRING(12) NULL,
     up_link          STRING     NULL,
     PRIMARY KEY (name, mac),
-    CONSTRAINT fk_floor FOREIGN KEY (build_short_name, floor_number) REFERENCES floors (build_short_name, number),
-    CONSTRAINT fk_switch FOREIGN KEY (up_switch_name, up_switch_mac) REFERENCES switches (name, mac)
+    CONSTRAINT fk_floor FOREIGN KEY (build_short_name, floor_number) REFERENCES floors (build_short_name, number) ON UPDATE CASCADE ON DELETE NO ACTION,
+    CONSTRAINT fk_switch FOREIGN KEY (up_switch_name, up_switch_mac) REFERENCES switches (name, mac) ON UPDATE CASCADE ON DELETE NO ACTION
 );
