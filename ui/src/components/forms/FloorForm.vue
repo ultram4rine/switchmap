43012f10e4fb05b6109875cdaebb38f809072e73
ultram4rine <ultramarine730@gmail.com>
2020-07-08 21:52:14 +0400

    removed unused computed import

--- ui/src/components/forms/FloorForm.vue
+++ ui/src/components/forms/FloorForm.vue
@@ -32,7 +32,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, computed } from "@vue/composition-api";
+import { defineComponent } from "@vue/composition-api";
 import { mdiClose } from "@mdi/js";
 
 import useInputValidator from "@/helpers/useInputValidator";
