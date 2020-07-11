37f65cfc210878b71cd93f28113ecb549abedd7c
ultram4rine <ultramarine730@gmail.com>
2020-07-11 22:35:47 +0400

    added vee-validate to login page

--- ui/src/views/Login.vue
+++ ui/src/views/Login.vue
@@ -1,33 +1,42 @@
 <template>
   <v-container class="fill-height" fluid>
     <v-row align="center" justify="center">
-      <v-col cols="12" sm="8" md="4">
+      <v-col cols="12">
         <v-card class="elevation-12">
           <v-toolbar dark flat>
             <v-toolbar-title>Login to SwitchMap</v-toolbar-title>
           </v-toolbar>
+
           <v-card-text>
-            <v-form>
-              <v-text-field
-                v-model="username"
-                label="Name"
-                type="text"
-                color="orange darken-1"
-                required
-                :prepend-icon="this.mdiAccount"
-              ></v-text-field>
-              <v-text-field
-                v-model="password"
-                label="Password"
-                :type="show ? 'text' : 'password'"
-                color="orange darken-1"
-                required
-                :prepend-icon="this.mdiKey"
-                :append-icon="show ? this.mdiEye : this.mdiEyeOff"
-                @click:append="show = !show"
-              ></v-text-field>
-            </v-form>
+            <ValidationObserver ref="observer" v-slot="{ validate }">
+              <v-form>
+                <ValidationProvider v-slot="{ errors }" name="Username" rules="required">
+                  <v-text-field
+                    v-model="username"
+                    label="Username"
+                    :error-messages="errors"
+                    color="orange darken-1"
+                    required
+                    :prepend-icon="this.mdiAccount"
+                  ></v-text-field>
+                </ValidationProvider>
+                <ValidationProvider v-slot="{ errors }" name="Password" rules="required">
+                  <v-text-field
+                    v-model="password"
+                    label="Password"
+                    :type="show ? 'text' : 'password'"
+                    :error-messages="errors"
+                    color="orange darken-1"
+                    required
+                    :prepend-icon="this.mdiKey"
+                    :append-icon="show ? this.mdiEye : this.mdiEyeOff"
+                    @click:append="show = !show"
+                  ></v-text-field>
+                </ValidationProvider>
+              </v-form>
+            </ValidationObserver>
           </v-card-text>
+
           <v-card-actions>
             <v-spacer />
             <v-btn color="orange darken-1" class="mr-4" @click="login">
@@ -45,7 +54,17 @@
 import Vue from "vue";
 import { mdiEye, mdiEyeOff, mdiAccount, mdiKey, mdiLogin } from "@mdi/js";
 
+import { ValidationObserver, ValidationProvider, extend } from "vee-validate";
+import { required } from "vee-validate/dist/rules";
+
+extend("required", {
+  ...required,
+  message: "{_field_} is required"
+});
+
 export default Vue.extend({
+  components: { ValidationObserver, ValidationProvider },
+
   data() {
     return {
       mdiEye: mdiEye,
@@ -63,15 +82,19 @@ export default Vue.extend({
 
   methods: {
     login: function() {
-      const { username, password } = this;
-      this.$store
-        .dispatch("auth/AUTH_LOGIN", { username, password })
-        .then(() => {
-          this.$router.push("/");
-        })
-        .catch((err: any) => {
-          console.log(err);
-        });
+      this.$refs.observer.validate().then((valid: boolean) => {
+        if (valid) {
+          const { username, password } = this;
+          this.$store
+            .dispatch("auth/AUTH_LOGIN", { username, password })
+            .then(() => {
+              this.$router.push("/");
+            })
+            .catch((err: any) => {
+              console.log(err);
+            });
+        }
+      });
     }
   }
 });
