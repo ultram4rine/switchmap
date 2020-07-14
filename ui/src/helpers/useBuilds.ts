90fc42dd62928eca007941be271c67857daf06e3
ultram4rine <ultramarine730@gmail.com>
2020-07-14 14:48:51 +0400

    moved api methods to useBuilds helper

--- ui/src/helpers/useBuilds.ts
+++ ui/src/helpers/useBuilds.ts
@@ -11,6 +11,8 @@ const buildEndpoint = (build: string) => {
 
 export default function () {
   const builds: Ref<Build[]> = ref([]);
+  const build: Ref<Build> = ref({} as Build);
+
   const buildForm = ref(false);
   const buildName = ref("");
   const buildShortName = ref("");
@@ -28,12 +30,65 @@ export default function () {
     }
   };
 
+  const getBuild = async (b: string) => {
+    try {
+      const resp = await axios.get<Build, AxiosResponse<Build>>(
+        buildEndpoint(b)
+      );
+      build.value = resp.data;
+    } catch (err) {
+      buildError.value = err;
+    }
+  };
+
+  const addBuild = async (name: string, shortName: string) => {
+    try {
+      axios.post(buildsEndpoint, {
+        name: name,
+        shortName: shortName,
+      });
+    } catch (err) {
+      buildError.value = err;
+    }
+  };
+
+  const updateBuild = async (
+    buildForUpdate: string,
+    name: string,
+    shortName: string
+  ) => {
+    try {
+      axios.put(buildEndpoint(buildForUpdate), {
+        name: name,
+        shortName: shortName,
+      });
+    } catch (err) {
+      buildError.value = err;
+    }
+  };
+
+  const deleteBuild = async (build: string) => {
+    try {
+      axios.delete(buildEndpoint(build));
+    } catch (err) {
+      buildError.value = err;
+    }
+  };
+
   return {
     builds,
+    build,
+
     buildForm,
     buildName,
     buildShortName,
+
     buildError,
+
     getAllBuilds,
+    getBuild,
+    addBuild,
+    updateBuild,
+    deleteBuild,
   };
 }
