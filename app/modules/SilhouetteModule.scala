bab834274fe5db2c20dfb05f27e731832b5bc13b
ultram4rine <ultramarine730@gmail.com>
2020-07-19 21:20:58 +0400

    added silhouette module

---
+++ app/modules/SilhouetteModule.scala
@@ -0,0 +1,168 @@
+package modules
+
+import auth.DefaultEnv
+import com.google.inject.name.Named
+import com.google.inject.{AbstractModule, Provides}
+import com.mohiva.play.silhouette.api.crypto.{
+  Crypter,
+  CrypterAuthenticatorEncoder,
+  Signer
+}
+import com.mohiva.play.silhouette.api.repositories.AuthInfoRepository
+import com.mohiva.play.silhouette.api.services.AuthenticatorService
+import com.mohiva.play.silhouette.api.{
+  Environment,
+  EventBus,
+  Silhouette,
+  SilhouetteProvider
+}
+import com.mohiva.play.silhouette.api.util.{
+  CacheLayer,
+  Clock,
+  FingerprintGenerator,
+  HTTPLayer,
+  IDGenerator,
+  PasswordHasherRegistry,
+  PlayHTTPLayer
+}
+import com.mohiva.play.silhouette.crypto.{
+  JcaCrypter,
+  JcaCrypterSettings,
+  JcaSigner,
+  JcaSignerSettings
+}
+import com.mohiva.play.silhouette.impl.authenticators.{
+  JWTAuthenticator,
+  JWTAuthenticatorService,
+  JWTAuthenticatorSettings
+}
+import com.mohiva.play.silhouette.impl.providers.CredentialsProvider
+import com.mohiva.play.silhouette.impl.providers.state.{
+  CsrfStateItemHandler,
+  CsrfStateSettings
+}
+import com.mohiva.play.silhouette.impl.util.{
+  DefaultFingerprintGenerator,
+  PlayCacheLayer,
+  SecureRandomIDGenerator
+}
+import com.typesafe.config.Config
+import javax.inject.Inject
+import net.ceedubs.ficus.readers.ValueReader
+import net.codingwell.scalaguice.ScalaModule
+import play.api.Configuration
+import play.api.libs.ws.WSClient
+import play.api.mvc.Cookie
+
+import scala.concurrent.ExecutionContext
+
+class SilhouetteModule @Inject() ()(implicit
+  ec: ExecutionContext
+) extends AbstractModule
+    with ScalaModule {
+
+  implicit val sameSiteReader: ValueReader[Option[Option[Cookie.SameSite]]] =
+    (config: Config, path: String) => {
+      if (config.hasPathOrNull(path)) {
+        if (config.getIsNull(path))
+          Some(None)
+        else {
+          Some(Cookie.SameSite.parse(config.getString(path)))
+        }
+      } else {
+        None
+      }
+    }
+
+  override def configure() {
+    bind[Silhouette[DefaultEnv]].to[SilhouetteProvider[DefaultEnv]]
+    // bind[UserService].to[UserServiceImpl]
+    // bind[UserDAO].to[UserDAOImpl]
+    bind[CacheLayer].to[PlayCacheLayer]
+    bind[IDGenerator].toInstance(new SecureRandomIDGenerator())
+    bind[FingerprintGenerator].toInstance(
+      new DefaultFingerprintGenerator(false)
+    )
+    bind[EventBus].toInstance(EventBus())
+    bind[Clock].toInstance(Clock())
+  }
+
+  @Provides
+  def provideHTTPLayer(client: WSClient): HTTPLayer = new PlayHTTPLayer(client)
+
+  @Provides
+  def provideEnvironment(
+    userService: UserService,
+    authenticatorService: AuthenticatorService[JWTAuthenticator],
+    eventBus: EventBus
+  ): Environment[DefaultEnv] = {
+    Environment[DefaultEnv](
+      userService,
+      authenticatorService,
+      Seq(),
+      eventBus
+    )
+  }
+
+  @Provides
+  @Named("csrf-state-item-signer")
+  def provideCSRFStateItemSigner(configuration: Configuration): Signer = {
+    val config = configuration.get[JcaSignerSettings](
+      "silhouette.csrfStateItemHandler.signer"
+    )
+
+    new JcaSigner(config)
+  }
+
+  @Provides
+  @Named("authenticator-signer")
+  def provideAuthenticatorSigner(configuration: Configuration): Signer = {
+    val config =
+      configuration.get[JcaSignerSettings]("silhouette.authenticator.signer")
+
+    new JcaSigner(config)
+  }
+
+  @Provides
+  @Named("authenticator-crypter")
+  def provideAuthenticatorCrypter(configuration: Configuration): Crypter = {
+    val config =
+      configuration.get[JcaCrypterSettings]("silhouette.authenticator.crypter")
+
+    new JcaCrypter(config)
+  }
+
+  @Provides
+  def provideAuthenticatorService(
+    @Named("authenticator-crypter") crypter: Crypter,
+    idGenerator: IDGenerator,
+    configuration: Configuration,
+    clock: Clock
+  ): AuthenticatorService[JWTAuthenticator] = {
+    val config =
+      configuration.get[JWTAuthenticatorSettings]("silhouette.authenticator")
+    val encoder = new CrypterAuthenticatorEncoder(crypter)
+
+    new JWTAuthenticatorService(config, None, encoder, idGenerator, clock)
+  }
+
+  @Provides
+  def provideCsrfStateItemHandler(
+    idGenerator: IDGenerator,
+    @Named("csrf-state-item-signer") signer: Signer,
+    configuration: Configuration
+  ): CsrfStateItemHandler = {
+    val settings =
+      configuration.get[CsrfStateSettings]("silhouette.csrfStateItemHandler")
+    new CsrfStateItemHandler(settings, idGenerator, signer)
+  }
+
+  @Provides
+  def provideCredentialsProvider(
+    authInfoRepository: AuthInfoRepository,
+    passwordHasherRegistry: PasswordHasherRegistry
+  ): CredentialsProvider = {
+    new CredentialsProvider(authInfoRepository, passwordHasherRegistry)
+  }
+
+}
