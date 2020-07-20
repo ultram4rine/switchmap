e4a00ef73d98a45208ac31686fd4790d062da16a
ultram4rine <ultramarine730@gmail.com>
2020-07-20 15:59:25 +0400

    reworked user service to ldap usage

--- app/services/UserService.scala
+++ app/services/UserService.scala
@@ -1,16 +1,6 @@
 package services
 
-import java.util.UUID
-
 import auth.User
-import com.mohiva.play.silhouette.api.LoginInfo
 import com.mohiva.play.silhouette.api.services.IdentityService
 
-import scala.concurrent.Future
-
-trait UserService extends IdentityService[User] {
-  def retrieveUserLoginInfo(
-    id: UUID,
-    providerID: String
-  ): Future[Option[(User, LoginInfo)]]
-}
+trait UserService extends IdentityService[User]
--- app/services/UserServiceImpl.scala
+++ app/services/UserServiceImpl.scala
@@ -1,26 +1,17 @@
 package services
 
-import java.util.UUID
-
 import auth.User
 import auth.ldap.LDAP
 import com.mohiva.play.silhouette.api.LoginInfo
-import javax.inject.Inject
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class UserServiceImpl @Inject() (ldap: LDAP)(implicit
-  ec: ExecutionContext
-) extends UserService {
-
-  def retrieve(loginInfo: LoginInfo): Future[Option[User]] =
-    userDAO.find(loginInfo)
-
-  def retrieveUserLoginInfo(
-    id: UUID,
-    providerID: String
-  ): Future[Option[(User, LoginInfo)]] = {
-    loginInfoDAO.find(id, providerID)
+class UserServiceImpl(ldap: LDAP)(implicit ec: ExecutionContext)
+    extends UserService {
+  override def retrieve(loginInfo: LoginInfo): Future[Option[User]] = {
+    ldap.getUserDN(loginInfo.providerID).map {
+      case Some(dn) => Some(User(dn))
+      case None     => None
+    }
   }
-
 }
