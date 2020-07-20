d03be4b08de39553b8a9a7eefda7e224b40bbac8
ultram4rine <ultramarine730@gmail.com>
2020-07-20 16:06:37 +0400

    user must extends from identity

--- app/auth/User.scala
+++ app/auth/User.scala
@@ -1,8 +1,9 @@
 package auth
 
+import com.mohiva.play.silhouette.api.Identity
 import play.api.libs.json.{Json, OFormat}
 
-case class User(username: String)
+case class User(username: String) extends Identity
 
 object User {
   implicit val userFormat: OFormat[User] = Json.format[User]
