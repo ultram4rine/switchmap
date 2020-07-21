a662861e93f970a5ed994a3d6dd59c702da39625
ultram4rine <ultramarine730@gmail.com>
2020-07-21 16:58:37 +0400

    fixed some of errors

--- app/modules/SilhouetteModule.scala
+++ app/modules/SilhouetteModule.scala
@@ -37,10 +37,15 @@ import com.mohiva.play.silhouette.impl.util.{
   PlayCacheLayer,
   SecureRandomIDGenerator
 }
+import com.mohiva.play.silhouette.password.{
+  BCryptPasswordHasher,
+  BCryptSha256PasswordHasher
+}
+import com.mohiva.play.silhouette.persistence.repositories.DelegableAuthInfoRepository
 import com.typesafe.config.Config
-import javax.inject.Inject
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+import net.ceedubs.ficus.readers.EnumerationReader._
 import net.ceedubs.ficus.readers.ValueReader
 import net.codingwell.scalaguice.ScalaModule
 import play.api.Configuration
@@ -48,13 +53,11 @@ import play.api.libs.ws.WSClient
 import play.api.mvc.Cookie
 import services.{UserService, UserServiceImpl}
 import utils.auth.DefaultEnv
+import utils.auth.ldap.{LDAP, LDAPProvider}
 
-import scala.concurrent.ExecutionContext
+import scala.concurrent.ExecutionContext.Implicits.global
 
-class SilhouetteModule @Inject() ()(implicit
-  ec: ExecutionContext
-) extends AbstractModule
-    with ScalaModule {
+class SilhouetteModule extends AbstractModule with ScalaModule {
 
   implicit val sameSiteReader: ValueReader[Option[Option[Cookie.SameSite]]] =
     (config: Config, path: String) => {
@@ -131,6 +134,11 @@ class SilhouetteModule @Inject() ()(implicit
   }
 
   @Provides
+  def provideAuthInfoRepository(): AuthInfoRepository = {
+    new DelegableAuthInfoRepository()
+  }
+
+  @Provides
   def provideAuthenticatorService(
     @Named("authenticator-crypter") crypter: Crypter,
     idGenerator: IDGenerator,
@@ -160,11 +168,24 @@ class SilhouetteModule @Inject() ()(implicit
   }
 
   @Provides
+  def providePasswordHasherRegistry(): PasswordHasherRegistry = {
+    PasswordHasherRegistry(
+      new BCryptSha256PasswordHasher(),
+      Seq(new BCryptPasswordHasher())
+    )
+  }
+
+  @Provides
   def provideCredentialsProvider(
     authInfoRepository: AuthInfoRepository,
     passwordHasherRegistry: PasswordHasherRegistry
   ): CredentialsProvider = {
     new CredentialsProvider(authInfoRepository, passwordHasherRegistry)
+  }
+
+  @Provides
+  def provideLDAPProvider(ldap: LDAP): LDAPProvider = {
+    new LDAPProvider(ldap)
   }
 
 }
