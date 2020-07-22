ff9d6b015e47300fce1ddd317a366cc0c764e9d0
ultram4rine <ultramarine730@gmail.com>
2020-07-22 22:24:54 +0400

    reworked floor model

--- src/main/scala/ru/sgu/switchmap/models/Floor.scala
+++ src/main/scala/ru/sgu/switchmap/models/Floor.scala
@@ -1,12 +1,5 @@
-package models
+package ru.sgu.switchmap.models
 
-import play.api.libs.json.{Json, OFormat}
+case class Floor(number: Int, buildName: String, buildShortName: String)
 
-case class Floor(number: Int, buildName: String, buildShortName: String) {
-  override def equals(that: Any): Boolean = false
-}
-
-object Floor {
-  def tupled: ((Int, String, String)) => Floor = (Floor.apply _).tupled
-  implicit val floorFormat: OFormat[Floor] = Json.format[Floor]
-}
+case object FloorNotFoundError
