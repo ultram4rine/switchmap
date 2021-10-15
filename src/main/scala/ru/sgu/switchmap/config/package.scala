dc66dee7ee52b2d692770e52f2312f66a9c82862
ultram4rine <ultramarine730@gmail.com>
2021-10-15 21:25:52 +0400

    add netdata server to conf

--- src/main/scala/ru/sgu/switchmap/config/package.scala
+++ src/main/scala/ru/sgu/switchmap/config/package.scala
@@ -1,18 +1,20 @@
 package ru.sgu.switchmap
 
 import pureconfig.ConfigSource
-import zio._
+import zio.{Has, ZIO, URIO, ULayer, ZLayer, Task}
 
 package object config {
   type Config = Has[APIConfig]
     with Has[DBConfig]
     with Has[LDAPConfig]
     with Has[String]
+    with Has[String]
 
   final case class AppConfig(
     api: APIConfig,
     db: DBConfig,
     ldap: LDAPConfig,
+    netdataServer: String,
     dnsSuffix: String
   )
 
@@ -33,6 +35,7 @@ package object config {
   val apiConfig: URIO[Has[APIConfig], APIConfig] = ZIO.access(_.get)
   val dbConfig: URIO[Has[DBConfig], DBConfig] = ZIO.access(_.get)
   val ldapConfig: URIO[Has[LDAPConfig], LDAPConfig] = ZIO.access(_.get)
+  val netdataServer: URIO[Has[String], String] = ZIO.access(_.get)
   val dnsSuffix: URIO[Has[String], String] = ZIO.access(_.get)
 
   object Config {
@@ -40,7 +43,11 @@ package object config {
     val live: ULayer[Config] = ZLayer.fromEffectMany(
       Task
         .effect(ConfigSource.default.loadOrThrow[AppConfig])
-        .map(c => Has(c.api) ++ Has(c.db) ++ Has(c.ldap) ++ Has(c.dnsSuffix))
+        .map(c =>
+          Has(c.api) ++ Has(c.db) ++ Has(c.ldap) ++ Has(c.netdataServer) ++ Has(
+            c.dnsSuffix
+          )
+        )
         .orDie
     )
   }
