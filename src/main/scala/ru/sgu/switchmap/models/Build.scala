4de7ce10bef34288bf9bf8fd5d6003612ac8bd00
ultram4rine <ultramarine730@gmail.com>
2021-10-21 23:54:33 +0400

    add default values for models

--- src/main/scala/ru/sgu/switchmap/models/Build.scala
+++ src/main/scala/ru/sgu/switchmap/models/Build.scala
@@ -1,12 +1,12 @@
 package ru.sgu.switchmap.models
 
-final case class DBBuild(name: String, shortName: String)
+final case class DBBuild(name: String = "", shortName: String = "")
 
 final case class Build(
-  name: String,
-  shortName: String,
-  floorsNumber: Long,
-  switchesNumber: Long
+  name: String = "",
+  shortName: String = "",
+  floorsNumber: Long = -1,
+  switchesNumber: Long = -1
 )
 
 final case class BuildNotFound(shortName: String) extends Exception
--- src/main/scala/ru/sgu/switchmap/models/Switch.scala
+++ src/main/scala/ru/sgu/switchmap/models/Switch.scala
@@ -1,20 +1,20 @@
 package ru.sgu.switchmap.models
 
 final case class Switch(
-  name: String,
-  ip: String,
-  mac: String,
-  snmpCommunity: String,
-  revision: Option[String],
-  serial: Option[String],
-  portsNumber: Option[Int],
-  buildShortName: Option[String],
-  floorNumber: Option[Int],
-  positionTop: Option[Float],
-  positionLeft: Option[Float],
-  upSwitchName: Option[String],
-  upSwitchMAC: Option[String],
-  upLink: Option[String]
+  name: String = "",
+  ip: String = "",
+  mac: String = "",
+  snmpCommunity: String = "",
+  revision: Option[String] = None,
+  serial: Option[String] = None,
+  portsNumber: Option[Int] = None,
+  buildShortName: Option[String] = None,
+  floorNumber: Option[Int] = None,
+  positionTop: Option[Float] = None,
+  positionLeft: Option[Float] = None,
+  upSwitchName: Option[String] = None,
+  upSwitchMAC: Option[String] = None,
+  upLink: Option[String] = None
 )
 
 final case class SwitchNotFound(name: String) extends Exception
