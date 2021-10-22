9bfbc13a00cfc28d71caf2af08de4ada7ef4326d
ultram4rine <ultramarine730@gmail.com>
2021-10-22 23:18:10 +0400

    add auth api

---
+++ ui/src/api/auth.ts
@@ -0,0 +1,22 @@
+import api from ".";
+
+import { authHeader } from "../helpers";
+
+export const login = async (
+  username: string,
+  password: string
+): Promise<void> => {
+  const resp = await api.post<{ token: string }>("/auth/login", {
+    username,
+    password,
+  });
+  if (resp.data.token) {
+    localStorage.setItem("token", resp.data.token);
+    api.defaults.headers.common["X-Auth-Token"] = authHeader();
+  }
+};
+
+export const logout = async (): Promise<void> => {
+  localStorage.removeItem("token");
+  api.defaults.headers.common["X-Auth-Token"] = authHeader();
+};
