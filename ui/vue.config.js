b19d1d64d075cb85df7383c423200674ccf0aa1a
ultram4rine <ultramarine730@gmail.com>
2021-10-24 00:54:06 +0400

    remove output dir

--- ui/vue.config.js
+++ ui/vue.config.js
@@ -2,7 +2,6 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-  outputDir: path.resolve(__dirname, "../public/ui"),
   assetsDir: process.env.NODE_ENV === "production" ? "static" : "",
   devServer: {
     public: "localhost:8080",
