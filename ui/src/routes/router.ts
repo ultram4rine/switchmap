58a380938d7236b14ace4189f22d7a23cf81903e
ultram4rine <ultramarine730@gmail.com>
2021-10-24 00:53:44 +0400

    use history mode

--- ui/src/routes/router.ts
+++ ui/src/routes/router.ts
@@ -1,5 +1,5 @@
 import Vue from "vue";
-import Router, { Route, Location } from "vue-router";
+import VueRouter, { Route, Location, RouteConfig } from "vue-router";
 
 import store from "../store";
 
@@ -10,55 +10,59 @@ import Floor from "@/views/Floor.vue";
 import Switches from "@/views/Switches.vue";
 import Vis from "@/views/Vis.vue";
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-const router = new Router({
-  routes: [
-    {
-      path: "/login",
-      name: "login",
-      component: Login,
-      meta: { skipIfAuth: true, layout: "empty" },
-    },
-    {
-      path: "/",
-      name: "root",
-      redirect: "/builds",
-      meta: { requiresAuth: true },
-    },
-    {
-      path: "/builds",
-      name: "home",
-      component: Builds,
-      meta: { requiresAuth: true, layout: "default" },
-    },
-    {
-      path: "/builds/:shortName",
-      name: "build",
-      component: Floors,
-      props: true,
-      meta: { requiresAuth: true, layout: "default" },
-    },
-    {
-      path: "/builds/:shortName/f:floor",
-      name: "floor",
-      component: Floor,
-      props: true,
-      meta: { requiresAuth: true, layout: "default" },
-    },
-    {
-      path: "/switches",
-      name: "switches",
-      component: Switches,
-      meta: { requiresAuth: true, layout: "default" },
-    },
-    {
-      path: "/vis",
-      name: "visualization",
-      component: Vis,
-      meta: { requiresAuth: true, layout: "default" },
-    },
-  ],
+const routes: Array<RouteConfig> = [
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+    meta: { skipIfAuth: true, layout: "empty" },
+  },
+  {
+    path: "/",
+    name: "root",
+    redirect: "/builds",
+    meta: { requiresAuth: true },
+  },
+  {
+    path: "/builds",
+    name: "home",
+    component: Builds,
+    meta: { requiresAuth: true, layout: "default" },
+  },
+  {
+    path: "/builds/:shortName",
+    name: "build",
+    component: Floors,
+    props: true,
+    meta: { requiresAuth: true, layout: "default" },
+  },
+  {
+    path: "/builds/:shortName/f:floor",
+    name: "floor",
+    component: Floor,
+    props: true,
+    meta: { requiresAuth: true, layout: "default" },
+  },
+  {
+    path: "/switches",
+    name: "switches",
+    component: Switches,
+    meta: { requiresAuth: true, layout: "default" },
+  },
+  {
+    path: "/vis",
+    name: "visualization",
+    component: Vis,
+    meta: { requiresAuth: true, layout: "default" },
+  },
+];
+
+const router = new VueRouter({
+  mode: "history",
+  base: process.env.BASE_URL,
+  routes,
 });
 
 router.beforeEach(
