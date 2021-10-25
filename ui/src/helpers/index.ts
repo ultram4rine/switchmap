b02ef76fe80cc1b5905c60900e74ef01900170b6
ultram4rine <ultramarine730@gmail.com>
2021-10-25 22:04:10 +0400

    add mac address normalization/denormalization

--- ui/src/helpers/index.ts
+++ ui/src/helpers/index.ts
@@ -2,3 +2,18 @@ export const authHeader = (): string => {
   const token = localStorage.getItem("token");
   return token ? token : "";
 };
+
+export const macNormalization = (mac: string): string | undefined => {
+  if (mac.length === 17) {
+    return mac.toLowerCase().replace(/:|-/g, "");
+  } else if (mac.length === 12) {
+    return mac.toLowerCase();
+  }
+};
+
+export const macDenormalization = (mac: string): string => {
+  return mac
+    .toUpperCase()
+    .replace(/(.{2})/g, "$1:")
+    .slice(0, -1);
+};
