bc685282f2bea133b8997d36e21236067c859e61
ultram4rine <ultramarine730@gmail.com>
2021-11-05 20:05:28 +0400

    use @ in imports

--- ui/src/components/cards/BuildCard.vue
+++ ui/src/components/cards/BuildCard.vue
@@ -39,7 +39,7 @@
 import { defineComponent, PropType } from "@vue/composition-api";
 import { mdiPencil, mdiDelete } from "@mdi/js";
 
-import { BuildResponse } from "../../types/build";
+import { BuildResponse } from "@/types/build";
 
 export default defineComponent({
   props: {
