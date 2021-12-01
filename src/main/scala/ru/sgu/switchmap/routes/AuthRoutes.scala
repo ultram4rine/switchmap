38cf2b7c952fbc492db02307e9f5d8962485ee39
ultram4rine <ultramarine730@gmail.com>
2021-12-02 00:21:56 +0400

    use tapir for auth routes

--- src/main/scala/ru/sgu/switchmap/routes/AuthRoutes.scala
+++ src/main/scala/ru/sgu/switchmap/routes/AuthRoutes.scala
@@ -6,10 +6,14 @@ import org.http4s.circe._
 import org.http4s.headers.`WWW-Authenticate`
 import org.http4s.{EntityDecoder, EntityEncoder, Challenge}
 import org.http4s.rho.RhoRoutes
+import sttp.tapir.json.circe._
+import sttp.tapir.generic.auto._
+import sttp.tapir.server.http4s.ztapir.ZHttp4sServerInterpreter
+import sttp.tapir.ztapir._
 import zio._
 import zio.interop.catz._
 
-import ru.sgu.switchmap.auth.Authenticator
+import ru.sgu.switchmap.auth.{Authenticator, AuthToken}
 import ru.sgu.switchmap.models.User
 
 final case class AuthRoutes[R <: Has[Authenticator]]() {
@@ -22,6 +26,21 @@ final case class AuthRoutes[R <: Has[Authenticator]]() {
     decoder: Encoder[A]
   ): EntityEncoder[AuthTask, A] =
     jsonEncoderOf[AuthTask, A]
+
+  var authEndpoint = endpoint.post
+    .in("auth" / "login")
+    .in(jsonBody[User])
+    .errorOut(stringBody)
+    .out(jsonBody[AuthToken])
+    .zServerLogic { user =>
+      Authenticator
+        .authenticate(user.username, user.password, user.rememberMe)
+        .mapError(_.toString())
+    }
+
+  val routes = ZHttp4sServerInterpreter()
+    .from(authEndpoint)
+    .toRoutes
 
   val api: RhoRoutes[AuthTask] =
     new RhoRoutes[AuthTask] {
