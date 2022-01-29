1e11555d02f2fbcb30f4ce99a890b83bdbd2b69f
ultram4rine <ultramarine730@gmail.com>
2022-01-29 21:42:30 +0400

    add build and floor cards

---
+++ svelte/src/components/cards/BuildCard.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import Card, { Actions, ActionButtons } from "@smui/card";
+  import Button, { Label } from "@smui/button";
+
+  import { Navigate } from "svelte-router-spa";
+
+  import type { BuildResponse } from "../../interfaces/build";
+
+  export let build: BuildResponse;
+</script>
+
+<Card variant="outlined">
+  <div style="padding: 1rem;">
+    <h2 class="mdc-typography--headline6" style="margin: 0;">
+      {build.name}
+    </h2>
+    <h3 class="mdc-typography--subtitle2" style="margin: 0; color: #888;">
+      {build.floorsNumber}
+      {build.floorsNumber !== 1 ? "floors" : "floor"},
+      {build.switchesNumber}
+      {build.switchesNumber !== 1 ? "switches" : "switch"}
+    </h3>
+  </div>
+  <Actions>
+    <ActionButtons>
+      <Button>
+        <Label>Add floor</Label>
+      </Button>
+      <Navigate to="builds/{build.shortName}">
+        <Button>
+          <Label>Go</Label>
+        </Button>
+      </Navigate>
+    </ActionButtons>
+  </Actions>
+</Card>
---
+++ svelte/src/components/cards/FloorCard.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import Card, { Actions, ActionButtons } from "@smui/card";
+  import Button, { Label } from "@smui/button";
+
+  import type { FloorResponse } from "../../interfaces/floor";
+
+  export let floor: FloorResponse;
+</script>
+
+<Card variant="outlined">
+  <div style="padding: 1rem;">
+    <h2 class="mdc-typography--headline6" style="margin: 0;">
+      Floor {floor.number}
+    </h2>
+    <h3 class="mdc-typography--subtitle2" style="margin: 0; color: #888;">
+      {floor.switchesNumber}
+      {floor.switchesNumber !== 1 ? "switches" : "switch"}
+    </h3>
+  </div>
+  <Actions>
+    <ActionButtons>
+      <Button>
+        <Label>Add switch</Label>
+      </Button>
+      <Button>
+        <Label>Go</Label>
+      </Button>
+    </ActionButtons>
+  </Actions>
+</Card>
